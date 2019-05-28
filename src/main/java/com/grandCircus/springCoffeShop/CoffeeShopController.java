6f9605ba2ba55ae970a323013f6c386ca3421b84
HoneyFinny <kuruvillamb@gmail.com>
2019-05-28 09:00:50 -0400

    firstcommit

---
+++ src/main/java/com/grandCircus/springCoffeShop/CoffeeShopController.java
@@ -0,0 +1,32 @@
+package com.grandCircus.springCoffeShop;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CoffeeShopController {
+	
+	@RequestMapping("/register-user")
+	public ModelAndView showUserForm() {
+		return new ModelAndView("user-form");
+	}
+	
+	@RequestMapping("/submit-user-registration")
+	public ModelAndView submitUserForm(
+			@RequestParam("firstname") String firstname,
+			@RequestParam("lastname") String lastname,
+			@RequestParam("password") String password) {
+		
+		// construct a user from the URL params
+		User user = new User();
+		user.setFirstname(firstname);
+		user.setLastname(lastname);
+		user.setPassword(password);
+		
+		ModelAndView mav = new ModelAndView("user-confirmation");
+		// send the whole user object to the JSP.
+		mav.addObject("user", user);
+		return mav;
+	}
+}
